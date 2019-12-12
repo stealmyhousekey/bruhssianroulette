8def3672c5c635797746344d1d07dab67f2c4639
Jacob Stelmachowske <jstelmachowske@gmail.com>
2019-12-11 19:15:50 -0500

    text improvements and multiplayer clarification
    -no multi without money, that's what i always say

--- BRUH/src/main/java/com/stelmo/brtest/SettingsActivity.kt
+++ BRUH/src/main/java/com/stelmo/brtest/SettingsActivity.kt
@@ -15,7 +15,8 @@ class SettingsActivity : AppCompatActivity() {
         val menuButton = findViewById(R.id.s_menu_button) as Button
         val infoString = "BRUHssian Roulette is a game revolving around the idea of funny, embarassing situations. \n \n" +
                 "In this demo, you can test your luck in a single-player game by pressing the SOLO button on the home screen. \n \n" +
-                "From here, you'll be presented with the game screen, where you can either spin the wheel or test your luck right away by pressing the READY button. \n \n" +
+                "From here, you'll be presented with the game screen, where you can either spin the wheel or press your luck right away by pressing the READY button. \n \n" +
+                "If none of the remaining slots feel safe, simply hit the RESET button to randomize the wheel and reset your odds to 1/6. \n \n" +
                 "If you lose, a random image from your phone will be displayed on-screen for all to see. \n \n" +
                 "So, you feeling lucky, bruh?"
 
